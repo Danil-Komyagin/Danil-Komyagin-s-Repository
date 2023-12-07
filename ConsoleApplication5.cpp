8397458943438022c25cacd19419cf525de8e88a
Danil-Komyagin <146763090+Danil-Komyagin@users.noreply.github.com>
2023-12-07 20:00:09 +0500

    laba2

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "RU");
+    int x, n;
+    int s = 0;
+    int i = 1;
+    int u = 1;
+
+    cout << "Введите х и количество элементов n" << endl;
+    cin >> x >> n;
+
+    while (i <= n) {
+        s = s + u;
+        u = u * x / i;
+        i += 1;
+    }
+    cout << s;
+    return 0;
+}
