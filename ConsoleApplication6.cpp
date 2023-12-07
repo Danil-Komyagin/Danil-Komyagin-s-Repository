e10fd1b0a90a1566da577039f592fa523a2e6570
Danil-Komyagin <146763090+Danil-Komyagin@users.noreply.github.com>
2023-12-07 21:15:42 +0500

    laba2

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    int max, sovp, num, n, a;
+    int i = 2;
+    cout << "Введите количество элементов" << endl;
+    cin >> n;
+    cout << "Вводите элементы последовательности" << endl;
+    cin >> a;
+    max = a;
+    sovp = 1;
+    num = 1;
+    while (i <= n) {
+        cin >> a;
+        if (a > max) { max = a; num = i; sovp = 1; }
+        else { if (a == max){ sovp += 1; } }
+        i += 1;
+    }
+    cout << "максимальное = " << max << endl;
+    cout << "количество совпадений = " << sovp << endl;
+    cout << "номер максимального = " << num << endl;
+    return 0;
+}
