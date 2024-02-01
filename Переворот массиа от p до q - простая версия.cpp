c12709d7c76c7a0b84ccb703d06328177687ee64
Danil-Komyagin <146763090+Danil-Komyagin@users.noreply.github.com>
2024-02-01 19:50:39 +0500

    Переворот массива простая версия

---
+++ Переворот массиа от p до q - простая версия.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+const int n = 10;
+int main() {
+	setlocale(LC_ALL, "RU");
+	srand(time(NULL));
+	int a[n];
+	int p, q, r;
+	cout << "Введите границы преобразования массива" << endl;
+	cin >> p >> q;
+	for (int i = 0; i < n; i++) {
+		a[i] = rand() % 200 - 100;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	int i = p - 1;
+	int j = q - 1;
+	while (i < j) {
+		r = a[i];
+		a[i] = a[j];
+		a[j] = r;
+		i++;
+		j--;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+	return 0;
+}
