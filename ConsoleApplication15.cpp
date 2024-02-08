65da4da72591f86008186c70f033bf72d636aaa8
Danil-Komyagin <146763090+Danil-Komyagin@users.noreply.github.com>
2024-02-08 23:42:17 +0500

    Add files via upload

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+void h(int n, int start, int end) {
+	if (n == 1) {
+		cout << "Перемещение 1 диска с " << start << " на " << end << endl;
+	}
+	else {
+		int temp = 6 - start - end;
+		h(n - 1, start, temp);
+		cout << "Перемещение " << n << "диска с " << start << " на " << end << endl;
+		h(n - 1, temp, end);
+	}
+}
+int main() {
+	setlocale(LC_ALL, "RU");
+	h(3, 1, 2);
+	return 0;
+}
