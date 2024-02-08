74d754b9cfcd4b40714c871ee11007935849978b
Danil-Komyagin <146763090+Danil-Komyagin@users.noreply.github.com>
2024-02-08 21:40:05 +0500

    Add files via upload

---
+++ Сумма ряда без рекурсии.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int x, n;
+	cin >> x >> n;
+	double s = 0;
+	for (int i = 0; i <= n; i+=1) {
+		int fact = 1;
+		for (int j = 1; j <= 2 * i + 1; j++) {
+			fact *= j;
+		}
+		s += pow(x, 2 * i + 1) / fact;
+	}
+	cout << s;
+	return 0;
+}
