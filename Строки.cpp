65e27d12dcc606469f5e0cbe4f623502ee8aac82
Danil-Komyagin <146763090+Danil-Komyagin@users.noreply.github.com>
2024-02-12 16:06:07 +0500

    Лаб 6

---
+++ Строки.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	system("chcp, 1251");
+	system("cls");
+	string str;
+	getline(cin, str);
+	char mass[] = "0123456789 qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+	int a = 0;    // Индекс первого элемента слова
+	int i = 0;	  // Переменная, использующаяся для перебора символов в слове
+	bool f = true;
+	while (str[i] != '\0') {  
+		for (int j = 0; (j < size(mass) - 1) && (f == true); j++) {  // Проверка текущего символа строки
+			if (str[i] == mass[j] || str[i] == ' ') f = false;
+		}
+		if (f == true) {				//Если символ не найден в массиве, удаляем слово до пробела
+			while (str[a] != ' ') {
+				str.erase(a, 1);
+			}
+			str.erase(a, 1); // Удаляем и пробел тоже
+			i = a;			// Индекс текущего символа равен индексу первого символа нового слова
+		}
+		else i++;           // Если символ найден среди разрешённых, переходим к следующему элементу
+		if (str[i] == ' ') {
+			a = i + 1;
+		}
+		f = true;
+	}
+	cout << str;
+	cout << size(mass);
+	return 0;
+}
