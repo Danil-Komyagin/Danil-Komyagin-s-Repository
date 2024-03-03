4a2d5a61daafdc0a333f27d46b09866dcbed7186
Danil-Komyagin <146763090+Danil-Komyagin@users.noreply.github.com>
2024-03-03 20:35:59 +0500

    Лабораторная работа 10

---
+++ 10.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	cout << "Введите длину массива и номер строки для добавления" << endl;
+	int kol, num;
+	cin >> kol >> num;
+	string copy;
+	string* mass = new string[kol];
+	string* mass2 = new string[kol + 1];
+	for (int i = 0; i < kol; i++){
+		cin >> *(mass + i);
+		if (i == num - 1) copy = *(mass + i);
+	}
+	for (int i = 0; i < kol; i++) {
+		*(mass2 + i) = *(mass + i);
+	}
+	*(mass2 + kol) = copy;
+	for (int i = 0; i <= kol; i++) {
+		cout << *(mass2 + i) << endl;
+	}
+	delete[] mass;
+	delete[] mass2;
+	return 0;
+}
+
+
