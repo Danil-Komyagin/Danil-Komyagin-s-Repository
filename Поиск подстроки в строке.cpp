1650fba5c9eada4a9608a6933389dc197ee54845
Danil-Komyagin <146763090+Danil-Komyagin@users.noreply.github.com>
2024-04-19 22:04:00 +0500

    Поиск подстроки в строке

---
+++ Поиск подстроки в строке.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include "Header.h"
+using namespace std;
+
+int main() {
+	system("chcp 1251 > Null");
+	string text, key;
+	cout << "Введите строку для поиска: " << endl;
+	getline(cin, text);
+	cout << "Введите образ для поиска: " << endl;
+	getline(cin, key);
+	pramoi(text, key);
+	kmp(text, key);
+	boyermoore(text, key);
+	return 0;
+}
